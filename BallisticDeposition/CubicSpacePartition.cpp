1c077153aeea8c3aa1ca59cf2b90bd80768475a7
gormadoc <shawnwwimer@gmail.com>
2023-07-07 12:30:30 -0500

    Correct gradient calculations.

--- BallisticDeposition/CubicSpacePartition.cpp
+++ BallisticDeposition/CubicSpacePartition.cpp
@@ -7,9 +7,33 @@ double local_potential(unsigned n, const double* x, double* grad, void* my_func_
 	float r2, r6 = 0;
 	float s6, s12 = 0;
 	float s6r6, s12r12 = 0;
+	float dx, dy = 0;
+
+	if (grad) {
+		for (int i = 0; i < 3; i++) {
+			grad[i] = 0;
+		}
+	}
+	
+	
 	for (int i = 0; i < positions->size(); i++) {
+		
 		auto p = (*positions)[i];
-		r2 = pow(p[0] - x[0], 2) + pow(p[1] - x[1], 2) + pow(p[2] - x[2], 2);
+		dx = p[0] - x[0];
+		dy = p[1] - x[1];
+		if (dx > 96 / 2) {
+			dx -= 96;
+		}
+		else if (dx < -96 / 2) {
+			dx += 96;
+		}
+		if (dy > 96 / 2) {
+			dy -= 96;
+		}
+		else if (dy < -96 / 2) {
+			dy += 96;
+		}
+		r2 = pow(dx, 2) + pow(dy, 2) + pow(p[2] - x[2], 2);
 		r6 = pow(r2, 3);
 		s6 = pow(2 * p[4], 6) / 2;
 		s12 = pow(s6, 2);
@@ -17,9 +41,9 @@ double local_potential(unsigned n, const double* x, double* grad, void* my_func_
 		s12r12 = pow(s6r6, 2);
 		val += s12r12 - s6r6;
 		if (grad) {
-			grad[0] += (6 * s6r6 / r2 - 12 * s12r12 / r2) * (x[0] - p[0]);
-			grad[1] += (6 * s6r6 / r2 - 12 * s12r12 / r2) * (x[1] - p[1]);
-			grad[2] += (6 * s6r6 / r2 - 12 * s12r12 / r2) * (x[2] - p[2]);
+			grad[0] += (6 * s6 * r6 - 12 * s12) / r6 / r6 / r2 * dx;
+			grad[1] += (6 * s6 * r6 - 12 * s12) / r6 / r6 / r2 * dy;
+			grad[2] += (6 * s6 * r6 - 12 * s12) / r6 / r6 / r2 * (x[2] - p[2]);
 		}
 	}
 	return val;
@@ -27,23 +51,32 @@ double local_potential(unsigned n, const double* x, double* grad, void* my_func_
 
 std::vector<double>* CubicSpacePartition::find_local_minimum(std::array<float, 3> position, float distance) {
 	// Get the bin
-	std::vector<int>* bin = find_nearest_bin(position);
+	int binidx = find_nearest_bin_idx(position);
+	std::vector<int>* bin = &bins[binidx];
 
 	// Build list of neighbor positions
 	std::vector<std::vector<float>> neighbors;
 	for (auto& p : *bin) {
 		neighbors.push_back((*atoms)[p]);
+		std::array<float, 3> npos = { (*atoms)[p][0], (*atoms)[p][1], (*atoms)[p][2] };
+		int nidx = find_nearest_bin_idx(npos);
+		if (nidx != binidx) {
+			nidx = nidx;
+		}
 	}
+
+	// Get the neighbor list
+	//std::vector<std::vector<float>*> neighbors = neighbor_lists[find_nearest_bin_idx(position)];
 
 	// Create function
 	opt.set_min_objective(local_potential, &neighbors);
 
 	// Set bounds
-	double x_min = position[0] - distance;
-	double y_min = position[1] - distance;
+	double x_min = position[0] - distance > 0 ? position[0] - distance : 0;
+	double y_min = position[1] - distance > 0 ? position[1] - distance : 0;
 	double z_min = position[2] - distance > 0 ? position[2] - distance : 0;
-	double x_max = position[0] + distance;
-	double y_max = position[1] + distance;
+	double x_max = position[0] + distance < L ? position[0] + distance : L;
+	double y_max = position[1] + distance < L ? position[1] + distance : L;
 	double z_max = position[2] + distance < H ? position[2] + distance : H;
 	opt.set_lower_bounds({ x_min, y_min, z_min });
 	opt.set_upper_bounds({ x_max, y_max, z_max });
--- BallisticDeposition/CubicSpacePartition.h
+++ BallisticDeposition/CubicSpacePartition.h
@@ -13,8 +13,9 @@ private:
 	float cube_size;
 	int bins_on_side, bins_in_set, bins_num;
 	std::vector<std::vector<int>> bins;
+	std::vector<std::vector<std::vector<float>*>> neighbor_lists;
 	std::vector<std::vector<float>>* atoms;
-	nlopt::opt opt = nlopt::opt(nlopt::LN_AUGLAG, 3);
+	nlopt::opt opt = nlopt::opt(nlopt::LD_AUGLAG, 3);
 	std::vector<double> minimization_result = { 0, 0, 0 };
 	
 public:
@@ -26,17 +27,28 @@ public:
 			std::vector<int>* b = new std::vector<int>;
 			bins.push_back(*b);
 			delete(b);
+			std::vector<std::vector<float>*>* n = new std::vector<std::vector<float>*>;
+			neighbor_lists.push_back(*n);
+			delete(n);
 		}
 
-		// Set stopping criteria
+		// Set optimizer parameters
 		opt.set_xtol_abs(1e-3);
 		opt.set_xtol_rel(1e-3);
 		opt.set_maxeval(400);
 		if (opt.get_algorithm() == nlopt::LN_AUGLAG) {
-			opt.set_local_optimizer(nlopt::opt(nlopt::LN_NELDERMEAD, 3));
+			nlopt::opt local = nlopt::opt(nlopt::LN_NELDERMEAD, 3);
+			local.set_xtol_abs(1e-3);
+			local.set_xtol_rel(1e-3);
+			local.set_maxeval(400);
+			opt.set_local_optimizer(local);
 		}
 		else if (opt.get_algorithm() == nlopt::LD_AUGLAG) {
-			opt.set_local_optimizer(nlopt::opt(nlopt::LD_MMA, 3));
+			nlopt::opt local = nlopt::opt(nlopt::LD_MMA, 3);
+			local.set_xtol_abs(1e-4);
+			local.set_xtol_rel(1e-4);
+			local.set_maxeval(400);
+			opt.set_local_optimizer(local);
 		}
 	}
 
@@ -44,32 +56,54 @@ public:
 		float nx = position[0] / cube_size;
 		float ny = position[1] / cube_size;
 		float nz = position[2] / cube_size;
-		int xidx = round(nx);
-		int yidx = round(ny);
-		int zidx = round(nz);
+		int xidx = floor(nx);
+		int yidx = floor(ny);
+		int zidx = floor(nz);
+		bool offz = nz > 1 / cube_size;
 
-		int xmod = xidx > nx ? xidx + 1 : xidx;
-		int ymod = yidx > ny ? yidx + 1 : yidx;
-		int zmod = zidx > nz ? zidx + 1 : zidx;
+		int xmod = xidx + (nx - xidx > 1 / cube_size ? 0 : -1);
+		int ymod = yidx + (ny - yidx > 1 / cube_size ? 0 : -1);
+		int zmod = zidx + (nz - zidx > 1 / cube_size ? 0 : -1);
 
 		if (xmod == bins_on_side) {
 			xmod = 0;
 		}
+		else if (xmod < 0) {
+			xmod = bins_on_side - 1;
+		}
 
 		if (ymod == bins_on_side) {
 			ymod = 0;
+		}
+		else if (ymod < 0) {
+			ymod = bins_on_side - 1;
+		}
+
+		if (zmod < 0) {
+			zmod = 0;
 		}
 
 		bins[zidx * bins_on_side * bins_on_side + yidx * bins_on_side + xidx].push_back(idx); // axis-aligned
 		bins[(zidx * bins_on_side * bins_on_side + yidx * bins_on_side + xmod) + bins_in_set].push_back(idx); // x-offset
 		bins[(zidx * bins_on_side * bins_on_side + ymod * bins_on_side + xidx) + bins_in_set * 2].push_back(idx); // y-offset
 		bins[(zidx * bins_on_side * bins_on_side + ymod * bins_on_side + xmod) + bins_in_set * 3].push_back(idx); // xy-offset
-		if (zmod > 0) {
+		if (offz) {
 			bins[(zmod * bins_on_side * bins_on_side + yidx * bins_on_side + xidx) + bins_in_set * 4].push_back(idx); // z-offset
 			bins[(zmod * bins_on_side * bins_on_side + yidx * bins_on_side + xmod) + bins_in_set * 5].push_back(idx); // xz-offset
 			bins[(zmod * bins_on_side * bins_on_side + ymod * bins_on_side + xidx) + bins_in_set * 6].push_back(idx); // yz-offset
 			bins[(zmod * bins_on_side * bins_on_side + ymod * bins_on_side + xmod) + bins_in_set * 7].push_back(idx); // xyz-offset
 		}
+
+		//neighbor_lists[zidx * bins_on_side * bins_on_side + yidx * bins_on_side + xidx].push_back(&((*atoms)[idx])); // axis-aligned
+		//neighbor_lists[(zidx * bins_on_side * bins_on_side + yidx * bins_on_side + xmod) + bins_in_set].push_back(&((*atoms)[idx])); // x-offset
+		//neighbor_lists[(zidx * bins_on_side * bins_on_side + ymod * bins_on_side + xidx) + bins_in_set * 2].push_back(&((*atoms)[idx])); // y-offset
+		//neighbor_lists[(zidx * bins_on_side * bins_on_side + ymod * bins_on_side + xmod) + bins_in_set * 3].push_back(&((*atoms)[idx])); // xy-offset
+		//if (zmod > 0) {
+		//	neighbor_lists[(zmod * bins_on_side * bins_on_side + yidx * bins_on_side + xidx) + bins_in_set * 4].push_back(&((*atoms)[idx])); // z-offset
+		//	neighbor_lists[(zmod * bins_on_side * bins_on_side + yidx * bins_on_side + xmod) + bins_in_set * 5].push_back(&((*atoms)[idx])); // xz-offset
+		//	neighbor_lists[(zmod * bins_on_side * bins_on_side + ymod * bins_on_side + xidx) + bins_in_set * 6].push_back(&((*atoms)[idx])); // yz-offset
+		//	neighbor_lists[(zmod * bins_on_side * bins_on_side + ymod * bins_on_side + xmod) + bins_in_set * 7].push_back(&((*atoms)[idx])); // xyz-offset
+		//}
 	}
 
 	int find_nearest_bin_idx(std::array<float, 3> position) {
@@ -83,44 +117,58 @@ public:
 		int factor = 0;
 		if (int(nx*2) % 2 == 1) {
 			if (int(ny*2) % 2 == 1) {
-				if (int(nz*2) % 2 == 1) {
-					factor = 0; // xyz-offset
+				if (fmod(nz, 1) == 0) {
+					factor = 4; // xyz-offset
 				}
 				else {
-					factor = 4; // xy-offset
+					factor = 0; // xy-offset
 				}
 			}
 			else {
-				if (int(nz*2) % 2 == 1) {
-					factor = 2; // xz-offset
+				if (fmod(nz, 1) == 0) {
+					factor = 6; // xz-offset
 				}
 				else {
-					factor = 6; // x-offset
+					factor = 2; // x-offset
 				}
 			}
 		}
 		else {
 			if (int(ny*2) % 2 == 1) {
-				if (int(nz*2) % 2 == 1) {
-					factor = 1; // yz-offset
+				if (fmod(nz, 1) == 0) {
+					factor = 5; // yz-offset
 				}
 				else {
-					factor = 5; // y-offset
+					factor = 1; // y-offset
 				}
 			}
 			else {
-				if (int(nz*2) % 2 == 1) {
-					factor = 3; // z-offset
+				if (fmod(nz, 1) == 0) {
+					factor = 7; // z-offset
 				}
 				else {
-					factor = 7;  // axis-aligned
+					factor = 3;  // axis-aligned
 				}
 			}
+		}
+
+		if (nz == 0) {
+			factor -= 4;
 		}
 
 		int xidx = int(ceil(nx - 1.f)) % bins_on_side;
 		int yidx = int(ceil(ny - 1.f)) % bins_on_side;
 		int zidx = int(ceil(nz - 1.f));
+		if (xidx < 0) {
+			xidx = bins_on_side - 1;
+		}
+		if (yidx < 0) {
+			yidx = bins_on_side - 1;
+		}
+		if (zidx < 0) {
+			zidx = 0;
+		}
+
 		return zidx * bins_on_side * bins_on_side + yidx * bins_on_side + xidx + bins_in_set * factor;
 	}
 
